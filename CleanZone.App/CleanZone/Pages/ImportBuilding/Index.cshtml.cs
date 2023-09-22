0f7a784e561c130d3bbf951b22f8a9511764432e
Jorge Toscano <jorgetoscanoalmeida13@gmail.com>
2023-09-22 15:22:15 +0100

    mecanismo de exceções

--- CleanZone.App/CleanZone/Pages/ImportBuilding/Index.cshtml.cs
+++ CleanZone.App/CleanZone/Pages/ImportBuilding/Index.cshtml.cs
@@ -17,12 +17,27 @@ public class IndexModel : PageModel
     {
         if (ResidencyYamlFile != null && ResidencyYamlFile.Length > 0)
         {
-            _importRepository.ImportBuildings(ResidencyYamlFile, User);
-            return RedirectToPage("/Index");
-
+            // Verifica a extensão do arquivo
+            if (Path.GetExtension(ResidencyYamlFile.FileName).ToLower() == ".yaml" || Path.GetExtension(ResidencyYamlFile.FileName).ToLower() == ".yml")
+            {
+                try
+                {
+                    _importRepository.ImportBuildings(ResidencyYamlFile, User);
+                    return RedirectToPage("/Index");
+                }
+                catch (Exception ex)
+                {
+                    TempData["ErrorMessage"] = "Failed to import the divisions. Check the YAML format.";
+                    return Page();
+                }
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "The file is not a valid YAML file.";
+                return Page();
+            }
         }
-        // Tratamento de erro, se a desserialização falhar ou não houver dados no YAML
-        TempData["ErrorMessage"] = "Failed to import the divisions. Check the YAML format.";
+        TempData["ErrorMessage"] = "No file selected for import.";
         return Page();
     }
 }
